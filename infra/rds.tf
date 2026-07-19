b361a44883f9df059ece08ed814e6b24b3972d6a
Light3313 <nkuharec73@gmail.com>
2026-07-19 16:46:29 +0300

    feat(infra): manage RDS master user password with AWS Secrets Manager

--- infra/rds.tf
+++ infra/rds.tf
@@ -25,9 +25,9 @@ resource "aws_db_instance" "main" {
   db_subnet_group_name                = aws_db_subnet_group.main.name
   vpc_security_group_ids              = [aws_security_group.sg_rds.id]
 
-  db_name  = "tasktracker"
-  username = "postgres"
-  password = var.db_master_password
+  db_name                     = "tasktracker"
+  username                    = "postgres"
+  manage_master_user_password = true # AWS Secrets Manager for the master password, so let AWS manage it
 
   # money saver (lab case)
   skip_final_snapshot = true
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,8 +1,3 @@
-variable "db_master_password" {
-  type      = string
-  sensitive = true
-}
-
 variable "app_image" {
   description = "full ECR image URI for the app container"
   type        = string
